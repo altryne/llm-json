4c635a33c59a761a8cb2c837fd2b05094692b09c
Alex Volkov <altryne@gmail.com>
2025-01-20 18:39:23 -0700

    New Readnme
    Signed-off-by: Alex Volkov <altryne@gmail.com>

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
-from setuptools import setup
+# setup.py
+import setuptools
 
-setup(
-    name="llm-json",
-    version="0.1.0",
-    packages=["llm_json"],
-    install_requires=[],
-    description="A JSON library wrapper that gracefully handles markdown-wrapped JSON from LLMs",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/altryne/llm-json",
-    author="Alex Volkov",
-    author_email="alex@alexw.me",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    extras_require={
-        'dev': [
-            'pytest>=7.0',
-        ],
-    },
-)
+if __name__ == "__main__":
+    setuptools.setup()
